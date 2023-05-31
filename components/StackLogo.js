feb03e7a415cbd7802352e1c2151e9f90ca7526b
focus85 <henokshiferaw@me.com>
2023-05-30 21:44:10 -0400

    Initial commit

--- components/StackLogo.js
+++ components/StackLogo.js
@@ -4,7 +4,7 @@ import styles from '../styles/JavaScriptLogo.module.css';
 const JavaScriptLogo = () => {
   return (
     <div className={styles.logo}>
-      TechIgniteWizard
+      CodeForge
     </div>
   );
 };
